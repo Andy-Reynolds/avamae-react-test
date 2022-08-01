4ec9af4d1ebd83348f002d45978bfffaec1d9532
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-08-01 15:38:14 +0100

    Added logo and links to nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Navbar.scss";
+import logo from "../../assets/svgs/Logo.svg";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img className="navbar__logo" src={logo} alt="" />
+      </Link>
+      <div className="navbar__links-container">
+        <Link className="navbar__link" to="/">
+          HOME
+        </Link>
+        <Link className="navbar__link" to="about">
+          ABOUT US
+        </Link>
+        <Link className="navbar__link" to="contact">
+          CONTACT US
+        </Link>
+        <button>Log in</button>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
