1400f281cc8cd13c52514394d1d13e6259b1af35
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-08-01 17:15:54 +0100

    footer done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <hr className="footer__line-break"></hr>
+      <p className="footer__paragraph">
+        Website Development by{" "}
+        <span className="footer__company-name">AVAMAE</span>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
