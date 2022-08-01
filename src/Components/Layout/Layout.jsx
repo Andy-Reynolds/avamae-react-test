06f753b04fb23e290aaf05f6a621a7f6296714b4
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-08-01 16:53:38 +0100

    Added footer component

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import "./Layout.scss";
 import Navbar from "../Navbar/Navbar.jsx";
+import Footer from "../Footer/Footer";
 
 const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Navbar />
       <main className="layout__content">{children}</main>
+      <Footer />
     </div>
   );
 };
