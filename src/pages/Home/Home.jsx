a2f1bde9a8321ec7719cb3be4a5437414c78a0ac
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-08-03 11:36:41 +0100

    Fetched home page data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Layout from "../../Components/Layout/Layout";
 
 const Home = () => {
+  const [homeObj, setHomeObj] = useState({});
+
+  const url =
+    "https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details";
+
+  const getData = async () => {
+    const result = await fetch(url);
+    const data = await result.json();
+    console.log(data);
+    setHomeObj(data);
+    console.log(homeObj);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Layout>
       <div>Home</div>
