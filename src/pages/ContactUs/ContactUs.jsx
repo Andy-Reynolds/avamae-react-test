ecdcc4f7a2571e8fbb689e5ca8b1a69932760463
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-08-04 18:29:10 +0100

    Started on contact page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import "./ContactUs.scss";
+import Form from "../../Components/Form/Form";
 import Layout from "../../Components/Layout/Layout";
+import companyLogo from "../../assets/images/Img_Contact.png";
 
 const ContactUs = () => {
   return (
     <Layout>
-      <div>Contact Us</div>
+      <div className="contact-us">
+        <div className="contact-us__content-container">
+          <h1 className="contact-us__heading">Contact Us</h1>
+          <p className="contact-us__paragraph">
+            Populo facilisi nam no, dolor deleniti deseruisse ne cum, nam quodsi
+            aliquam eligendi ne. Ferri euismod accusata te nec, summo accumsan
+            at vix.
+          </p>
+          <Form />
+        </div>
+        <img
+          className="contact-us__logo"
+          src={companyLogo}
+          alt="Company Logo"
+        />
+      </div>
     </Layout>
   );
 };
