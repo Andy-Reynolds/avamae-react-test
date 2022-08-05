e4338a3a59093f6f9fb9864ebaa4329b81ab5913
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-08-05 12:39:58 +0100

    Added unstyled form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,7 +1,75 @@
+import { useState } from "react";
 import "./Form.scss";
 
 const Form = () => {
-  return <div className="form">Form</div>;
+  const [showAddress, setShowAddress] = useState(false);
+
+  const handleAddressClick = () => {
+    setShowAddress(!showAddress);
+    console.log(showAddress);
+  };
+
+  return (
+    <form className="form">
+      <div>
+        <label htmlFor="name">Full name</label>
+        <input type="text" id="name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email address</label>
+        <input type="text" id="email" />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone number 01 - optional</label>
+        <input type="text" id="phone" />
+      </div>
+      <button>Add new phone number</button>
+      <div>
+        <label htmlFor="message">
+          Message - Maximum text length is 500 characters
+        </label>
+        <input type="text" id="message" />
+      </div>
+      <div>
+        <input type="checkbox" id="add-address" onChange={handleAddressClick} />
+        <label htmlFor="add-address">Add address details</label>
+      </div>
+
+      {showAddress && (
+        <div className="address-c     ontainer">
+          <div className="adddress-line-container">
+            <div>
+              <label htmlFor="address-one">Address line 1</label>
+              <input type="text" id="address-one" />
+            </div>
+            <div>
+              <label htmlFor="address-two">Address line 2</label>
+              <input type="text" id="address-two" />
+            </div>
+          </div>
+          <div className="adddress-extras-container">
+            <div>
+              <label htmlFor="city">City/Town</label>
+              <input type="text" id="city" />
+            </div>
+            <div>
+              <label htmlFor="state">State/County</label>
+              <input type="text" id="state" />
+            </div>
+            <div>
+              <label htmlFor="postcode">Postcode</label>
+              <input type="text" id="postcode" />
+            </div>
+            <div>
+              <label htmlFor="country">Country</label>
+              <input type="text" id="country" />
+            </div>
+          </div>
+        </div>
+      )}
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default Form;
