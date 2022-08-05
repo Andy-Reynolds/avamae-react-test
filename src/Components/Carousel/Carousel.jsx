a3778c5904cf2a8c5ef103f1a09e029ea02e2bb6
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-08-05 16:25:26 +0100

    fixed log in section image

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -8,9 +8,7 @@ import Button from "../Button/Button";
 // import { Link } from "react-router-dom";
 // import homeImage from "../../assets/images/shutterstock_407632243.jpg";
 
-const Carousel = ({ homeObj, buttonLink, buttonText }) => {
-  // console.log(homeObj.Details[0].Title);
-
+const Carousel = ({ homeObj }) => {
   return (
     <Swiper
       className="carousel"
@@ -52,6 +50,24 @@ const Carousel = ({ homeObj, buttonLink, buttonText }) => {
           <img
             className="carousel__image"
             src={homeObj.Details[1].ImageUrl}
+            alt=""
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="carousel__slide">
+          <div className="carousel__gradient">
+            <div className="carousel__content">
+              <h2 className="carousel__title">{homeObj.Details[2].Title}</h2>
+              <h3 className="carousel__subtitle">
+                {homeObj.Details[2].Subtitle}
+              </h3>
+              <Button buttonLink={"/contact"} buttonText={"Contact Us"} />
+            </div>
+          </div>
+          <img
+            className="carousel__image"
+            src={homeObj.Details[2].ImageUrl}
             alt=""
           />
         </div>
